67e09b485e841fdf811075c98aa7c7f016a7b94f
Jose Platero <contact@joseplatero.com>
2013-03-04 20:44:24 -0500

    new posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,5 +5,19 @@ def index
 @posts  = Post.all
 end
 
+def new
+
+@post = Post.new
+
+end
+
+def create
+		@post = Post.new(params[:post])
+		if @post.save
+			redirect_to posts_path
+		else 
+			render :new
+		end
+	end
 
 end
